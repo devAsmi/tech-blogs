d238c0d64cd4b4e27ca4a3aef092d76521fbfd9a
devasmi <impokhrelasmita5@gmail.com>
2023-02-21 19:18:11 -0500

    fixed logic to always render homepage

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,18 +1,21 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  if (!req.session.logged_in) {
-    res.redirect("/login");
-  } else {
-    res.redirect("/homepage");
-  }
+  res.redirect("/homepage");
 });
 
 router.get("/homepage", (req, res) => {
+  res.render("homepage", {
+    logged_in: req.session.logged_in,
+    user_id: req.session.user_id,
+  });
+});
+
+router.get("/dashboard", (req, res) => {
   if (!req.session.logged_in) {
     res.redirect("/login");
   } else {
-    res.render("homepage", {
+    res.render("dashboard", {
       logged_in: req.session.logged_in,
       user_id: req.session.user_id,
     });
